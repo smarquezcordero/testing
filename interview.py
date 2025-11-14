fc89acdfb930fa9f7a20de3afa9499f62a8dcaa2
smarquezcordero <smarquezcordero@AUGUSTA.EDU>
2025-11-14 16:11:10 -0500

    Add files via upload

---
+++ interview.py
@@ -0,0 +1,11 @@
+
+nums = []
+
+nums.append(1)
+nums.append(2)
+nums.append(3)
+
+print(nums)
+
+for i in range(1):
+    print(nums[::-1])
---
+++ test2.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+
+def mean(nums):
+    return sun(nums)/len(nums)
+
+arr = [5, 10, 15, 20]
+
+
+math = mean(arr)
+
+print(math)
